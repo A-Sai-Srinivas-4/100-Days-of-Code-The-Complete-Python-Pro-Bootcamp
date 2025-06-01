dc26ddd2560cb029e35ffe21cc3b8d2407828781
SaiSrinivas-4 <saisrinivas.asapu@aifalabs.com>
2025-06-01 12:51:23 +0530

    Day-18 Turtle,shapes,hirst painting Challenges are completed.

---
+++ Day-18 Turtle Challenge & Graphics (GUI)/Turtle Challenge/main.py
@@ -0,0 +1,27 @@
+from turtle import Turtle,Screen
+
+tim = Turtle()
+
+tim.shape("turtle")
+
+# Challenge - 1
+for _ in range(5):
+    tim.forward(100)
+    tim.left(90)
+
+# Challenge - 2
+for _ in range(25):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+# Challenge - 3
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+

8759fb9f9987ece2accf984c74cbc11aa3592330
SaiSrinivas-4 <saisrinivas.asapu@aifalabs.com>
2025-06-01 18:11:11 +0530

    Day-19 Turtles Race Project

---
+++ Day-19 Sketch App using Instances, State, Higher Order Functions and Event Listeners/Sketch APP Challenge/main.py
@@ -0,0 +1,33 @@
+import turtle as t
+
+tim = t.Turtle()
+screen = t.Screen()
+
+def move_forword():
+    tim.forward(10)
+
+def move_backword():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forword)
+screen.onkey(key="s", fun=move_backword)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear_screen)
+
+screen.exitonclick()
